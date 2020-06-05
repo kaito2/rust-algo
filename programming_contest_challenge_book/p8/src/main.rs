b76b15c9c022233ff830564fe5327abfc8fab67b
kaito2 <kaito.iwatsuki@nislab.doshisha.ac.jp>
2020-06-06 00:44:54 +0900

    p8 done

--- programming_contest_challenge_book/p8/src/main.rs
+++ programming_contest_challenge_book/p8/src/main.rs
@@ -1,9 +1,20 @@
 fn main() {
-    let m = 30;
-    let k = vec![1, 2, 3, 4, 5];
+    let m = 40;
+    let k = vec![1, 2, 3, 4, 10];
     println!("{}", if lottery(m, k) { "yes" } else { "no" });
 }
 
 fn lottery(m: u32, k: Vec<u32>) -> bool {
-    true
+    for ka in &k {
+        for kb in &k {
+            for kc in &k {
+                for kd in &k {
+                    if (ka + kb + kc + kd) == m {
+                        return true;
+                    }
+                }
+            }
+        }
+    }
+    false
 }
