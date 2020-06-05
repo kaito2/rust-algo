abb55b07ed8c06edfc13aac28c4335cf426744c9
kaito2 <kaito.iwatsuki@nislab.doshisha.ac.jp>
2020-06-06 00:40:41 +0900

    p8 frame

---
+++ programming_contest_challenge_book/README.md
@@ -0,0 +1,3 @@
+## なに
+
+[プログラミングコンテストチャレンジブック 第2版 | マイナビブックス](https://book.mynavi.jp/ec/products/detail/id=22672)いわゆる蟻本をrustで解いていく
---
+++ programming_contest_challenge_book/p8/src/main.rs
@@ -0,0 +1,9 @@
+fn main() {
+    let m = 30;
+    let k = vec![1, 2, 3, 4, 5];
+    println!("{}", if lottery(m, k) { "yes" } else { "no" });
+}
+
+fn lottery(m: u32, k: Vec<u32>) -> bool {
+    true
+}
