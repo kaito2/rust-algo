e7e878522063b894e0b74b483147b438949bcfbb
kaito2 <kaito.iwatsuki@nislab.doshisha.ac.jp>
2020-06-08 00:14:36 +0900

    ant complete

--- programming_contest_challenge_book/p23/src/main.rs
+++ programming_contest_challenge_book/p23/src/main.rs
@@ -1,101 +1,37 @@
+use std::cmp;
+
 fn main() {
-    let ant = Ant::new(30, true);
-    println!("{:?}", ant);
-
-    let mut ants = Ants::new(vec![Ant::new(30, true), Ant::new(31, false)], 100);
-    println!("{:?}", ants);
-    &ants.move_1step();
-    println!("{:?}", ants);
+    println!("hello, world!");
 }
 
-#[derive(Debug)]
-struct Ant {
-    distance_from_left: i32,
-    is_facing_right: bool,
+fn calc_max_ant(rod_length: i32, ant_positions: &Vec<i32>) -> i32 {
+    let mut max = 0;
+    for ant_position in ant_positions {
+        let current_ant_max = cmp::max(*ant_position, rod_length - ant_position);
+        max = cmp::max(max, current_ant_max);
+    }
+    max
 }
 
-impl Ant {
-    pub fn new(distance_from_left: i32, is_facing_right: bool) -> Ant {
-        Ant {
-            distance_from_left,
-            is_facing_right,
-        }
+fn calc_min_ant(rod_length: i32, ant_positions: &Vec<i32>) -> i32 {
+    let mut min = 0;
+    for ant_position in ant_positions {
+        let current_ant_max = cmp::min(*ant_position, rod_length - ant_position);
+        min = cmp::max(min, current_ant_max);
     }
-
-    fn move_delta(&self) -> i32 {
-        if self.is_facing_right {
-            1
-        } else {
-            -1
-        }
-    }
-
-    fn next_distance_from_left(&self) -> i32 {
-        self.distance_from_left + &self.move_delta()
-    }
-
-    fn move_position(&mut self) {
-        self.distance_from_left = self.next_distance_from_left();
-    }
-
-    fn reverse_direction(&mut self) {
-        self.is_facing_right = !self.is_facing_right;
-    }
-}
-
-#[derive(Debug)]
-struct Ants {
-    ants: Vec<Ant>,
-    rod_length: i32,
-}
-
-impl Ants {
-    pub fn new(ants: Vec<Ant>, rod_length: i32) -> Ants {
-        Ants { ants, rod_length }
-    }
-
-    fn move_1step(&mut self) {
-        let mut i = 0;
-        loop {
-            if i + 1 >= self.ants.len() {
-                break;
-            }
-            // if i+1 == ants.len() の場合の分岐
-            if Ants::judge_collision(&self.ants[i], &self.ants[i + 1]) {
-                if let Some(ant) = self.ants.get_mut(i) {
-                    ant.reverse_direction();
-                }
-                if let Some(ant) = self.ants.get_mut(i + 1) {
-                    ant.reverse_direction();
-                }
-                i += 1;
-            } else {
-                if let Some(ant) = self.ants.get_mut(i) {
-                    ant.move_position();
-                }
-                if let Some(ant) = self.ants.get_mut(i + 1) {
-                    ant.move_position();
-                }
-            }
-            i += 1;
-        }
-    }
-
-    fn judge_collision(ant1: &Ant, ant2: &Ant) -> bool {
-        ant1.next_distance_from_left() == ant2.distance_from_left
-            && ant1.distance_from_left == ant2.next_distance_from_left()
-    }
+    min
 }
 
 #[cfg(test)]
 mod tests {
-    use super::Ant;
-    use super::Ants;
+    use super::calc_max_ant;
+    use super::calc_min_ant;
 
     #[test]
-    fn test_collision_detect() {
-        let ant1 = Ant::new(30, true);
-        let ant2 = Ant::new(31, false);
-        assert_eq!(Ants::judge_collision(&ant1, &ant2), true);
+    fn it_works() {
+        let rod_length = 10;
+        let ant_positions = vec![2, 6, 7];
+        assert_eq!(calc_min_ant(rod_length, &ant_positions), 4);
+        assert_eq!(calc_max_ant(rod_length, &ant_positions), 8);
     }
 }
